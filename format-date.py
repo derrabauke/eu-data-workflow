ff335fd0577bcc133e3d24caf7c41c099374e29a
Automated <actions@users.noreply.github.com>
2022-07-03 12:06:55 +0000

    data(h2020): Sun Jul  3 12:06:55 UTC 2022

--- format-date.py
+++ format-date.py
@@ -11,12 +11,13 @@ if len(sys.argv) < 2 :
   raise("Missing file path.")
 
 def customParser(date):
-  return pd.to_datetime(date, infer_datetime_format=True, dayfirst=True, format="%Y-%m-%d %H:%M:%S")
+  res = pd.to_datetime(date, infer_datetime_format=True, dayfirst=True)
+  return res
 
 
 with open(sys.argv[1], "r", newline="") as f, open(os.path.splitext(sys.argv[1])[0] + "_date_formatted.csv", "w", newline="") as out:
   # convert string of column indices to list of integers
-  columns = [int(i) for i in sys.argv[2].split("\n")]
+  columns = [int(i) for i in sys.argv[2].split(",")]
   # parse dates in given columns
   csv = pd.read_csv(f, quotechar='"', parse_dates=columns, date_parser=customParser)
   # write output
